0d8b433a00d25c87f450a1f06572936df4e9dec1
sgasse <sgasse@users.noreply.github.com>
2019-10-22 11:25:45 +0200

    Set seasonal backgrounds

--- changeBG.sh
+++ changeBG.sh
@@ -1,13 +1,56 @@
 #!/bin/bash
 
+function setWinter {
+    rm bg.jpg
+    cp bg_ice_small.jpg bg.jpg
+}
+
+function setSpring {
+    rm bg.jpg
+    cp bg_lawn_small.jpg bg.jpg
+}
+
+function setSummer {
+    rm bg.jpg
+    cp bg_sea_small.jpg bg.jpg
+}
+
+function setFall {
+    rm bg.jpg
+    cp bg_field_small.jpg bg.jpg
+}
+
+
+month=$(date +%m)
 
 while [ 1 ]
 do
     cd /var/www/simongasse.de/img
-    rm bg.jpg
-    cp bg_ice_small.jpg bg.jpg
-    sleep 20s
-    rm bg.jpg
-    cp bg_sea_small.jpg bg.jpg
-    sleep 20s
+    if [ "$month" == "1" ]; then
+        setWinter
+    elif [ "$month" == "2" ]; then
+        setWinter
+    elif [ "$month" == "3" ]; then
+        setWinter
+    elif [ "$month" == "4" ]; then
+        setSpring
+    elif [ "$month" == "5" ]; then
+        setSpring
+    elif [ "$month" == "6" ]; then
+        setSummer
+    elif [ "$month" == "7" ]; then
+        setSummer
+    elif [ "$month" == "8" ]; then
+        setSummer
+    elif [ "$month" == "9" ]; then
+        setFall
+    elif [ "$month" == "10" ]; then
+        setFall
+    elif [ "$month" == "11" ]; then
+        setWinter
+    elif [ "$month" == "12" ]; then
+        setWinter
+    fi
+    sleep 3d
 done
+
--- startup.sh
+++ startup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-echo "Starting nginx"
+echo "Starting personal homepage container"
+
+# start nginx in foreground if only command to keep container alive
 #nginx -g 'daemon off;'
+
+# start nginx in the background
 nginx
+
+# change background image according to season, keep container alive
 /bin/bash /root/changeBG.sh
